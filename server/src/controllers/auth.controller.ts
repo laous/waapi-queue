ef949bca93eb500ee5dc1f31f203726d3784cec4
Oussama Lamnaouer <lmnouer59@gmail.com>
2023-11-06 14:15:29 +0100

    set up basic auth using jwt

---
+++ server/src/controllers/auth.controller.ts
@@ -0,0 +1,61 @@
+import { Request, Response } from 'express';
+import bcrypt from 'bcryptjs';
+import jwt from 'jsonwebtoken';
+import prisma from '../config/prisma';
+
+const JWT_SECRET = process.env.JWT_SECRET;
+
+export const register = async (req: Request, res: Response) => {
+  try {
+    const { email, password }: { email: string; password: string } = req.body;
+
+    const existingUser = await prisma.user.findUnique({
+      where: { email: email },
+    });
+    if (existingUser) {
+      return res.status(400).json({ message: 'Email is already registered' });
+    }
+
+    const hashedPassword = await bcrypt.hash(password, 10);
+
+    const user = await prisma.user.create({
+      data: {
+        email,
+        password: hashedPassword,
+        queue: {
+          create: {},
+        },
+      },
+    });
+
+    const token = jwt.sign({ userId: user.id, email: user.email }, JWT_SECRET);
+
+    res.status(201).json({ user, token });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: 'Internal Server Error' });
+  }
+};
+
+export const login = async (req: Request, res: Response) => {
+  try {
+    const { email, password } = req.body;
+
+    const user = await prisma.user.findUnique({ where: { email } });
+    if (!user) {
+      return res.status(401).json({ message: 'Invalid credentials' });
+    }
+
+    const passwordMatch = await bcrypt.compare(password, user.password);
+    if (!passwordMatch) {
+      return res.status(401).json({ message: 'Invalid credentials' });
+    }
+
+    const token = jwt.sign({ userId: user.id, email: user.email }, JWT_SECRET);
+
+    res.status(200).json({ user, token });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: 'Internal Server Error' });
+  }
+};
--- server/src/index.ts
+++ server/src/index.ts
@@ -1,6 +1,7 @@
 import express, { Request, Response } from 'express';
 import cors from 'cors';
 import dotenv from 'dotenv';
+import authRouter from './routes/auth.route';
 
 dotenv.config();
 
@@ -11,9 +12,14 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cors());
 
-app.get('/', (req: Request, res: Response) => {
+const globalRouter = express.Router();
+app.use('/api/v1', globalRouter);
+
+globalRouter.get('/', (req: Request, res: Response) => {
   res.send('Hello, Waapi!');
 });
+
+globalRouter.use('/auth', authRouter);
 
 app.listen(port, () => {
   console.log(`Server running at http://localhost:${port}`);
